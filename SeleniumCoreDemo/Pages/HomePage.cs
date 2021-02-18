02d271efe046033ddb86fdfe2c2168909e8c6812
Grace Tôrres Leite <grace.torres@dbserver.com.br>
2021-02-18 14:59:03 -0300

    add pages

---
+++ SeleniumCoreDemo/Pages/HomePage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCoreDemo.Pages
+{
+    public class HomePage
+    {
+        public HomePage(IWebDriver webDriver)
+        {
+            Driver = webDriver;
+        }
+
+        private IWebDriver Driver { get; }
+        public IWebElement linkLogin => Driver.FindElement(By.LinkText("Login"));
+
+        IWebElement linkEmployeeDetails => Driver.FindElement(By.LinkText("Employee Details"));
+
+        public void ClickLogin() => linkLogin.Click();
+
+        public bool IsEmployeeDetails() => linkEmployeeDetails.Displayed;
+
+
+    }
+}
---
+++ SeleniumCoreDemo/Pages/LoginPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCoreDemo.Pages
+{
+    class LoginPage
+    {
+        public LoginPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public IWebDriver Driver { get; }
+
+        IWebElement textUserName => Driver.FindElement(By.Name("UserName"));
+
+        IWebElement textPassword => Driver.FindElement(By.Name("Password"));
+
+        IWebElement btnLogin => Driver.FindElement(By.XPath("//input[@value='Log in']"));
+
+        public void Login(string userName, string password)
+        {
+            textUserName.SendKeys(userName);
+            textPassword.SendKeys(password);
+        }
+    }
+}
